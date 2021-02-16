81e8a4004a6cb96ffb952051fd01e63e0923430a
adeprianto21 <adeprianto21@upi.edu>
2021-02-16 13:26:37 +0000

    Update Database Config

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
             //   'ERROR_FOR_DIVISION_BY_ZERO',
             //   'NO_AUTO_CREATE_USER',
             //   'NO_ENGINE_SUBSTITUTION'
-            'ONLY_FULL_GROUP_BY',
-            'STRICT_TRANS_TABLES',
-            'NO_ZERO_IN_DATE',
-            'NO_ZERO_DATE',
-            'ERROR_FOR_DIVISION_BY_ZERO',
-            'NO_ENGINE_SUBSTITUTION',
+            //   'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION',
             ],
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
