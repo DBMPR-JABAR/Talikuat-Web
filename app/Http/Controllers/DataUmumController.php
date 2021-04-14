3105f067c7b2761da8db34e84c1e222ca98b1243
Ade Prianto <adeprianto21@gmail.com>
2021-04-14 19:55:15 +0700

    Update Get Data Umum For Mobile

--- app/Http/Controllers/DataUmumController.php
+++ app/Http/Controllers/DataUmumController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
@@ -15,11 +16,6 @@ class DataUmumController extends Controller
   {
 
     $result = DB::table('data_umum')
-      ->selectRaw('aksi, data_umum.id, konsultan_supervisi, lat_akhir, lat_awal, long_akhir, long_awal, nama_gs, nama_kegiatan, nama_ppk, ruas_jalan, nama_se, nilai_kontrak, no_kontrak, no_spmk, opd, panjang, pemda, penyedia_jasa, pk, ppk, rab, rab1, rab2, rab3, GROUP_CONCAT(ruas_jalan) as ruas_jalan_concat, satuan_panjang, satuan_waktu, data_umum_ruas.segmen_jalan, sk, sm, tgl_input, tgl_kontrak, tgl_spmk, tgl_update, ul_jadual, ul_jaminan, ul_rencana, ul_spek, ul_spkmp, ul_spl, ul_spmk, ul_sppbj, unit, kantor.nama_kantor as unor, data_umum.user as user, waktu_pelaksanaan, wp')
-      ->join('data_umum_ruas', 'data_umum.id', '=', 'data_umum_ruas.id')
-      ->join('kantor', 'data_umum.unor', '=', 'kantor.id_kantor')
-      ->groupBy('data_umum.id')
-      ->orderBy('data_umum.id', 'desc')
       ->limit(5)
       ->get();
 
@@ -36,14 +32,8 @@ class DataUmumController extends Controller
     $keyword = $request->query("keyword");
 
     $result = DB::table('data_umum')
-      ->selectRaw('aksi, data_umum.id, konsultan_supervisi, lat_akhir, lat_awal, long_akhir, long_awal, nama_gs, nama_kegiatan, nama_ppk, ruas_jalan, nama_se, nilai_kontrak, no_kontrak, no_spmk, opd, panjang, pemda, penyedia_jasa, pk, ppk, rab, rab1, rab2, rab3, GROUP_CONCAT(ruas_jalan) as ruas_jalan_concat, satuan_panjang, satuan_waktu, data_umum_ruas.segmen_jalan, sk, sm, tgl_input, tgl_kontrak, tgl_spmk, tgl_update, ul_jadual, ul_jaminan, ul_rencana, ul_spek, ul_spkmp, ul_spl, ul_spmk, ul_sppbj, unit, kantor.nama_kantor as unor, data_umum.user as user, waktu_pelaksanaan, wp')
-      ->join('data_umum_ruas', 'data_umum.id', '=', 'data_umum_ruas.id')
-      ->join('kantor', 'data_umum.unor', '=', 'kantor.id_kantor')
-      ->groupBy('data_umum.id')
-      ->having('data_umum.nama_kegiatan', 'like', '%' . $keyword . '%')
-      ->orHaving('ruas_jalan_concat', 'like', '%' . $keyword . '%')
-      ->orHaving('unor', 'like', '%' . $keyword . '%')
-      ->orHaving('data_umum.id', $keyword)
+      ->where('nm_paket', 'like', '%' . $keyword . '%')
+      ->orWhere('unor', 'like', '%' . $keyword . '%')
       ->paginate(15);
 
     return response()->json([
@@ -74,7 +64,7 @@ class DataUmumController extends Controller
       "pemda" => "required",
       "opd" => "required",
       "unor" => "required",
-      "nm_paket"=>"required",
+      "nm_paket" => "required",
       "kategori" => "required",
       "no_kontrak" => "required",
       "tgl_kontrak" => "required",
@@ -122,67 +112,65 @@ class DataUmumController extends Controller
       "nm_ppk" => $request->input("nama_ppk"),
       "nm_se" => $request->input("nama_se"),
       "nm_gs" => $request->input("nama_gs"),
-      "is_adendum"=>0,
-      "created_at"=>\Carbon\Carbon::now()
+      "is_adendum" => 0,
+      "created_at" => \Carbon\Carbon::now()
     ];
 
     $insertedDataUmumId = DB::table('data_umum')->insertGetId($data);
 
-
-
     // List Ruas Jalan
-    if($request->list_data_umum_ruas){
-    $listDataUmumRuas = json_decode($request->input('list_data_umum_ruas'));
+    if ($request->list_data_umum_ruas) {
+      $listDataUmumRuas = json_decode($request->input('list_data_umum_ruas'));
 
-    foreach ($listDataUmumRuas as $dataUmumRuas) {
-      if (
-        !isNotNullOrBlank($dataUmumRuas->ruas_jalan) ||
-        !isNotNullOrBlank($dataUmumRuas->segmen_jalan) ||
-        !isNotNullOrBlank($dataUmumRuas->lat_awal) ||
-        !isNotNullOrBlank($dataUmumRuas->long_awal) ||
-        !isNotNullOrBlank($dataUmumRuas->lat_akhir) ||
-        !isNotNullOrBlank($dataUmumRuas->long_akhir)
-      ) {
-        return response()->json([
-          'status' => 'failed',
-          'code' => '400',
-          'error' => 'Data Umum Ruas Ada Yang Kosong Atau Null'
-        ], Response::HTTP_BAD_REQUEST);
+      foreach ($listDataUmumRuas as $dataUmumRuas) {
+        if (
+          !isNotNullOrBlank($dataUmumRuas->ruas_jalan) ||
+          !isNotNullOrBlank($dataUmumRuas->segmen_jalan) ||
+          !isNotNullOrBlank($dataUmumRuas->lat_awal) ||
+          !isNotNullOrBlank($dataUmumRuas->long_awal) ||
+          !isNotNullOrBlank($dataUmumRuas->lat_akhir) ||
+          !isNotNullOrBlank($dataUmumRuas->long_akhir)
+        ) {
+          return response()->json([
+            'status' => 'failed',
+            'code' => '400',
+            'error' => 'Data Umum Ruas Ada Yang Kosong Atau Null'
+          ], Response::HTTP_BAD_REQUEST);
+        }
+      }
+
+
+      foreach ($listDataUmumRuas as $dataUmumRuas) {
+        DB::table('data_umum_ruas')->insert([
+          "id_data_umum" => $insertedDataUmumId, // diganti sesuai DB
+          "ruas_jalan" => $dataUmumRuas->ruas_jalan,
+          "segmen_jalan" => $dataUmumRuas->segmen_jalan,
+          "lat_awal" => $dataUmumRuas->lat_awal,
+          "long_awal" => $dataUmumRuas->long_awal,
+          "lat_akhir" => $dataUmumRuas->lat_akhir,
+          "long_akhir" => $dataUmumRuas->long_akhir,
+          "created_at" => \Carbon\Carbon::now()
+        ]);
+      }
+      return response()->json([
+        'status' => 'success',
+        'code' => '200',
+        'result' => $data
+      ]);
+    } else {
+      for ($i = 0; $i < count($request->ruas_jalan); $i++) {
+        DB::table("data_umum_ruas")->insert([
+          "id_data_umum" => $insertedDataUmumId,
+          "ruas_jalan" => $request->ruas_jalan[$i],
+          "segment_jalan" => $request->segmen_jalan[$i],
+          "lat_awal" => $request->lat_awal[$i],
+          "long_awal" => $request->long_awal[$i],
+          "lat_akhir" => $request->lat_akhir[$i],
+          "long_akhir" => $request->long_akhir[$i],
+          "created_at" => \Carbon\Carbon::now()
+        ]);
       }
     }
- 
-
-    foreach ($listDataUmumRuas as $dataUmumRuas) {
-      DB::table('data_umum_ruas')->insert([
-        "id_data_umum" => $insertedDataUmumId, // diganti sesuai DB
-        "ruas_jalan" => $dataUmumRuas->ruas_jalan,
-        "segmen_jalan" => $dataUmumRuas->segmen_jalan,
-        "lat_awal" => $dataUmumRuas->lat_awal,
-        "long_awal" => $dataUmumRuas->long_awal,
-        "lat_akhir" => $dataUmumRuas->lat_akhir,
-        "long_akhir" => $dataUmumRuas->long_akhir,
-        "created_at"=>\Carbon\Carbon::now()
-      ]);
-    }
-    return response()->json([
-      'status' => 'success',
-      'code' => '200',
-      'result' => $data
-    ]);
- }else{
-   for ($i=0; $i <count($request->ruas_jalan) ; $i++) { 
-     DB::table("data_umum_ruas")->insert([
-      "id_data_umum"=>$insertedDataUmumId,
-      "ruas_jalan"=>$request->ruas_jalan[$i],
-      "segment_jalan"=>$request->segmen_jalan[$i],
-      "lat_awal"=>$request->lat_awal[$i],
-      "long_awal"=>$request->long_awal[$i],
-      "lat_akhir"=>$request->lat_akhir[$i],
-      "long_akhir"=>$request->long_akhir[$i],
-      "created_at"=>\Carbon\Carbon::now()
-     ]);
-   }
- }
     $data['id'] = $insertedDataUmumId;
     // $data['list_data_umum_ruas'] = $listDataUmumRuas;
 
@@ -215,7 +203,8 @@ class DataUmumController extends Controller
     ]);
   }
 
-  public function updateDataUmum(Request $req){
+  public function updateDataUmum(Request $req)
+  {
 
     date_default_timezone_set('Asia/Jakarta');
     $data = [
@@ -238,21 +227,21 @@ class DataUmumController extends Controller
       "nm_ppk" => $req->input("nm_ppk"),
       "nm_se" => $req->input("nm_se"),
       "nm_gs" => $req->input("nm_gs"),
-      "is_adendum"=>0,
-      "updated_at"=>\Carbon\Carbon::now()
+      "is_adendum" => 0,
+      "updated_at" => \Carbon\Carbon::now()
     ];
-    DB::table('data_umum')->where('id',$req->id)->update($data);
-    DB::table('data_umum_ruas')->where('id_data_umum',$req->id)->delete();
-    for ($i=0; $i <count($req->ruas_jalan) ; $i++) { 
+    DB::table('data_umum')->where('id', $req->id)->update($data);
+    DB::table('data_umum_ruas')->where('id_data_umum', $req->id)->delete();
+    for ($i = 0; $i < count($req->ruas_jalan); $i++) {
       DB::table("data_umum_ruas")->insert([
-       "id_data_umum"=>$req->id,
-       "ruas_jalan"=>$req->ruas_jalan[$i],
-       "segment_jalan"=>$req->segmen_jalan[$i],
-       "lat_awal"=>$req->lat_awal[$i],
-       "long_awal"=>$req->long_awal[$i],
-       "lat_akhir"=>$req->lat_akhir[$i],
-       "long_akhir"=>$req->long_akhir[$i],
-       "created_at"=>\Carbon\Carbon::now()
+        "id_data_umum" => $req->id,
+        "ruas_jalan" => $req->ruas_jalan[$i],
+        "segment_jalan" => $req->segmen_jalan[$i],
+        "lat_awal" => $req->lat_awal[$i],
+        "long_awal" => $req->long_awal[$i],
+        "lat_akhir" => $req->lat_akhir[$i],
+        "long_akhir" => $req->long_akhir[$i],
+        "created_at" => \Carbon\Carbon::now()
       ]);
     }
     return response()->json([
@@ -261,11 +250,12 @@ class DataUmumController extends Controller
       'result' => "oke"
     ]);
   }
-  public function addAdendum(Request $req){
-    $get_data = DB::table('data_umum')->where('id',$req->id)->first();
+  public function addAdendum(Request $req)
+  {
+    $get_data = DB::table('data_umum')->where('id', $req->id)->first();
     $data = [
       // Data Umum
-      'id_data_umum'=>$req->id,
+      'id_data_umum' => $req->id,
       'pemda' => $get_data->pemda,
       "opd" => $get_data->opd,
       "unor" => $get_data->unor,
@@ -284,88 +274,87 @@ class DataUmumController extends Controller
       "nm_ppk" => $get_data->nm_ppk,
       "nm_se" => $get_data->nm_se,
       "nm_gs" => $get_data->nm_gs,
-      "adendum"=>"Adendum 1",
-      "created_at"=>\Carbon\Carbon::now()
+      "adendum" => "Adendum 1",
+      "created_at" => \Carbon\Carbon::now()
     ];
     DB::table('data_umum_adendum')->insert($data);
-      DB::table('data_umum')->where("id",$req->id)->update([
-        "is_adendum"=>1
-      ]);
+    DB::table('data_umum')->where("id", $req->id)->update([
+      "is_adendum" => 1
+    ]);
 
-    $get_data_ruas = DB::table('data_umum_ruas')->where('id_data_umum',$req->id)->get();
+    $get_data_ruas = DB::table('data_umum_ruas')->where('id_data_umum', $req->id)->get();
 
-    foreach($get_data_ruas as $data){
+    foreach ($get_data_ruas as $data) {
       DB::table('data_umum_ruas_adendum')->insert([
-        "id_data_umum_adendum"=>$data->id_data_umum,
-        "ruas_jalan"=>$data->ruas_jalan,
-        "segment_jalan"=>$data->segment_jalan,
-        "lat_awal"=>$data->lat_awal,
-        "long_awal"=>$data->long_awal,
-        "lat_akhir"=>$data->lat_akhir,
-        "long_akhir"=>$data->long_akhir,
-        "adendum"=>"Adendum 1",
-        "created_at"=>\Carbon\Carbon::now()
+        "id_data_umum_adendum" => $data->id_data_umum,
+        "ruas_jalan" => $data->ruas_jalan,
+        "segment_jalan" => $data->segment_jalan,
+        "lat_awal" => $data->lat_awal,
+        "long_awal" => $data->long_awal,
+        "lat_akhir" => $data->lat_akhir,
+        "long_akhir" => $data->long_akhir,
+        "adendum" => "Adendum 1",
+        "created_at" => \Carbon\Carbon::now()
       ]);
     }
-      
-      return response()->json([
-        "code"=>200
-      ],200);
+
+    return response()->json([
+      "code" => 200
+    ], 200);
   }
 
 
   public function AddNewAdendum(Request $req)
   {
-    $get_data = DB::table('data_umum_adendum')->where('id_data_umum',$req->id)->get();
-      DB::table('data_umum_adendum')->where([
-        ["is_adendum",null],
-        ["id_data_umum","=",$req->id]
-        ])->update([
-        "is_adendum"=>1
-        ]);
-      $data = [
-        // Data Umum
-        'id_data_umum'=>$req->id,
-        'adendum'=>"Adendum ".count($get_data)+1,
-        'pemda' => $get_data[0]->pemda,
-        "opd" => $get_data[0]->opd,
-        "unor" => $get_data[0]->unor,
-        "kategori" => $get_data[0]->kategori,
-        "nm_paket" => $get_data[0]->nm_paket,
-        "no_kontrak" => $get_data[0]->no_kontrak,
-        "tgl_kontrak" => $get_data[0]->tgl_kontrak,
-        "nilai_kontrak" => $get_data[0]->nilai_kontrak,
-        "no_spmk" => $get_data[0]->no_spmk,
-        "tgl_spmk" => $get_data[0]->tgl_spmk,
-        "panjang_km" => $get_data[0]->panjang_km,
-        "lama_waktu" => $get_data[0]->lama_waktu,
-        "ppk" => $get_data[0]->ppk,
-        "penyedia" => $get_data[0]->penyedia,
-        "konsultan" => $get_data[0]->konsultan,
-        "nm_ppk" => $get_data[0]->nm_ppk,
-        "nm_se" => $get_data[0]->nm_se,
-        "nm_gs" => $get_data[0]->nm_gs,
-        "created_at"=>\Carbon\Carbon::now()
-      ];
-      DB::table('data_umum_adendum')->insert($data);
-      $get_data_ruas = DB::table('data_umum_ruas')->where('id_data_umum',$req->id)->get();
+    $get_data = DB::table('data_umum_adendum')->where('id_data_umum', $req->id)->get();
+    DB::table('data_umum_adendum')->where([
+      ["is_adendum", null],
+      ["id_data_umum", "=", $req->id]
+    ])->update([
+      "is_adendum" => 1
+    ]);
+    $data = [
+      // Data Umum
+      'id_data_umum' => $req->id,
+      'adendum' => "Adendum " . count($get_data) + 1,
+      'pemda' => $get_data[0]->pemda,
+      "opd" => $get_data[0]->opd,
+      "unor" => $get_data[0]->unor,
+      "kategori" => $get_data[0]->kategori,
+      "nm_paket" => $get_data[0]->nm_paket,
+      "no_kontrak" => $get_data[0]->no_kontrak,
+      "tgl_kontrak" => $get_data[0]->tgl_kontrak,
+      "nilai_kontrak" => $get_data[0]->nilai_kontrak,
+      "no_spmk" => $get_data[0]->no_spmk,
+      "tgl_spmk" => $get_data[0]->tgl_spmk,
+      "panjang_km" => $get_data[0]->panjang_km,
+      "lama_waktu" => $get_data[0]->lama_waktu,
+      "ppk" => $get_data[0]->ppk,
+      "penyedia" => $get_data[0]->penyedia,
+      "konsultan" => $get_data[0]->konsultan,
+      "nm_ppk" => $get_data[0]->nm_ppk,
+      "nm_se" => $get_data[0]->nm_se,
+      "nm_gs" => $get_data[0]->nm_gs,
+      "created_at" => \Carbon\Carbon::now()
+    ];
+    DB::table('data_umum_adendum')->insert($data);
+    $get_data_ruas = DB::table('data_umum_ruas')->where('id_data_umum', $req->id)->get();
 
-    foreach($get_data_ruas as $data){
+    foreach ($get_data_ruas as $data) {
       DB::table('data_umum_ruas_adendum')->insert([
-        "id_data_umum_adendum"=>$data->id_data_umum,
-        "ruas_jalan"=>$data->ruas_jalan,
-        "segment_jalan"=>$data->segment_jalan,
-        "lat_awal"=>$data->lat_awal,
-        "long_awal"=>$data->long_awal,
-        "lat_akhir"=>$data->lat_akhir,
-        "long_akhir"=>$data->long_akhir,
-        "adendum"=>"Adendum ".count($get_data)+1,
-        "created_at"=>\Carbon\Carbon::now()
+        "id_data_umum_adendum" => $data->id_data_umum,
+        "ruas_jalan" => $data->ruas_jalan,
+        "segment_jalan" => $data->segment_jalan,
+        "lat_awal" => $data->lat_awal,
+        "long_awal" => $data->long_awal,
+        "lat_akhir" => $data->lat_akhir,
+        "long_akhir" => $data->long_akhir,
+        "adendum" => "Adendum " . count($get_data) + 1,
+        "created_at" => \Carbon\Carbon::now()
       ]);
     }
     return response()->json([
-      "code"=>200
-    ],200);
-    
+      "code" => 200
+    ], 200);
   }
 }
