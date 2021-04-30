4fc820bc63772e0cca1c765281c837c8f78991d4
Ade Prianto <adeprianto21@upi.edu>
2021-04-30 14:07:16 +0700

    Adding AuthController & Login For Authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The table associated with the model.
@@ -39,5 +40,13 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'pass',
+        'id_sesi'
+    ];
+
+    protected $fillable = [
+        'user',
+        'pass',
+        'id_member',
+        'level'
     ];
 }
