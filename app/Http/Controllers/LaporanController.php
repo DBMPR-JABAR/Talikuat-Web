fd27e15ac9b3931a4aa6eeb1f16b00712a0c22c1
xart22 <fajar.rn22@gmail.com>
2021-06-30 02:51:55 +0700

    adding feature delelte request v.1.2

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -532,17 +532,29 @@ class LaporanController extends Controller
     }
   }
 
-  public function deleteLaporan($id)
+  public function deleteLaporan(Request $req)
   {
-    DB::table('master_laporan_harian')->where('no_trans',$id)->delete();
-    DB::table('detail_laporan_harian_pekerjaan')->where('no_trans',$id)->delete();
-    DB::table('detail_laporan_harian_bahan')->where('no_trans',$id)->delete();
-    DB::table('detail_laporan_harian_tkerja')->where('no_trans',$id)->delete();
-    DB::table('detail_laporan_harian_cuaca')->where('no_trans',$id)->delete();
-    DB::table('detail_laporan_harian_beton')->where('no_trans',$id)->delete();
-    DB::table('detail_laporan_harian_hotmix')->where('no_trans',$id)->delete();
-    DB::table('detail_laporan_harian_peralatan')->where('no_trans',$id)->delete();
-    DB::table('detail_laporan_harian_tkerja')->where('no_trans',$id)->delete();
+    $validator = Validator::make($req->all(), [
+      "alasan"=>"required"
+    ]);
+    if ($validator->fails()) {
+      return response()->json([
+        'status' => 'failed',
+        'code' => 400,
+        'error' => $validator->getMessageBag()->getMessages()
+      ], 400);
+    }
+    DB::table('master_laporan_harian')->where('no_trans',$req->id)->update([
+      'reason_delete'=>$req->alasan
+    ]);
+    // DB::table('detail_laporan_harian_pekerjaan')->where('no_trans',$id)->delete();
+    // DB::table('detail_laporan_harian_bahan')->where('no_trans',$id)->delete();
+    // DB::table('detail_laporan_harian_tkerja')->where('no_trans',$id)->delete();
+    // DB::table('detail_laporan_harian_cuaca')->where('no_trans',$id)->delete();
+    // DB::table('detail_laporan_harian_beton')->where('no_trans',$id)->delete();
+    // DB::table('detail_laporan_harian_hotmix')->where('no_trans',$id)->delete();
+    // DB::table('detail_laporan_harian_peralatan')->where('no_trans',$id)->delete();
+    // DB::table('detail_laporan_harian_tkerja')->where('no_trans',$id)->delete();
 
     return response()->json([
       'code'=>200
