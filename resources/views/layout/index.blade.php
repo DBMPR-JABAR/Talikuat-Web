982a52acce63212708a840712789c5a5b0f5bee2
rival2905 <valdothea@gmail.com>
2021-07-26 10:20:22 +0700

    table wrapper

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -57,6 +57,35 @@
             type="text/css"
             href="{{ asset('vendor/datatables.min.css') }}"
         />
+        <style>
+            
+            @media only screen and (max-width: 646px) {
+            .table-striped {
+                width: 40%;
+                font-size: 12px;
+                overflow-x:scroll;
+            }
+            #table-wrapper {
+            position:relative;
+            }
+            #table-scroll {
+            overflow:auto;  
+            margin-top:20px;
+            }
+            #table-wrapper table {
+            width:100%;
+            }
+            
+            #table-wrapper table thead th .text {
+            position:absolute;   
+            top:-20px;
+            z-index:2;
+            width:35%;
+            border:1px solid red;
+            }
+           
+        }
+        </style>
         @yield('header')
     </head>
     <body>
