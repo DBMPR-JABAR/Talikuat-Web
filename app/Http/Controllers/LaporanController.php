022067fbde805cee132d681cbfe112e9d9974fbe
xart22 <fajar.rn22@gmail.com>
2021-08-30 23:17:08 +0700

    fixing laporan try catch

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -282,140 +282,151 @@ class LaporanController extends Controller
         $file = $req->file('soft');
         $name = time() . "_" . $file->getClientOriginalName();
         $getTeam = DB::table('request')->where('id', $req->permohonan)->first();
-        $id = DB::table('master_laporan_harian')->insertGetId([
-            "real_date" => \Carbon\Carbon::now(),
-            "user" => $req->user,
-            "kegiatan" => $req->kegiatan,
-            "unor" => $req->unor,
-            "ruas_jalan" => $req->ruas_jalan,
-            "tanggal" => $req->tanggal,
-            "segmen_jalan" => $req->segmen_jalan,
-            "id_request" => $req->permohonan,
-            "tgl_input" => \Carbon\Carbon::now(),
-            "nama_kontraktor" => $req->kontraktor,
-            "nama_ppk" => $req->ppk,
-            "nama_konsultan" => $req->konsultan,
-            "volume" => $req->volume,
-            "satuan" => $req->satuan,
-            "nmp" => $req->jenis_pekerjaan,
-            "ket" => $req->ket,
-            "gambar" => $this->PATH_FILE_DB . "/" . $name,
-            "id_jadual" => $req->id_jadual,
-            "id_data_umum" => $req->id_data_umum,
-            "bobot" => $req->bobot,
-            "field_team_konsultan" => $getTeam->field_team_konsultan
-        ]);
-        Storage::putFileAs($this->PATH_FILE_DB, $file, $name);
-        DB::table('detail_laporan_harian_pekerjaan')->insert([
-            "no_trans" => $id,
-            "no_pekerjaan" => $req->no_pekerjaan,
-            "jenis_pekerjaan" => $req->jenis_pekerjaan,
-            "sta_awal" => $req->sta_awal,
-            "sta_akhir" => $req->sta_akhir,
-            "ki_ka" => $req->ki_ka,
-            "volume" => $req->volume,
-            "satuan" => $req->satuan,
-            "ket" => $req->ket,
-            "tgl" => $req->tanggal,
-            "bobot" => $req->bobot
-        ]);
-
-        if ($req->bahan[0] != null) {
-            for ($i = 0; $i < count($req->bahan); $i++) {
-                DB::table('detail_laporan_harian_bahan')->insert([
-                    "no_trans" => $id,
-                    "bahan" => $req->bahan[$i],
-                    "volume" => $req->volume_bahan[$i],
-                    "satuan" => $req->satuan_bahan[$i]
-                ]);
+        DB::beginTransaction();
+        try {
+            $id = DB::table('master_laporan_harian')->insertGetId([
+                "real_date" => \Carbon\Carbon::now(),
+                "user" => $req->user,
+                "kegiatan" => $req->kegiatan,
+                "unor" => $req->unor,
+                "ruas_jalan" => $req->ruas_jalan,
+                "tanggal" => $req->tanggal,
+                "segmen_jalan" => $req->segmen_jalan,
+                "id_request" => $req->permohonan,
+                "tgl_input" => \Carbon\Carbon::now(),
+                "nama_kontraktor" => $req->kontraktor,
+                "nama_ppk" => $req->ppk,
+                "nama_konsultan" => $req->konsultan,
+                "volume" => $req->volume,
+                "satuan" => $req->satuan,
+                "nmp" => $req->jenis_pekerjaan,
+                "ket" => $req->ket,
+                "gambar" => $this->PATH_FILE_DB . "/" . $name,
+                "id_jadual" => $req->id_jadual,
+                "id_data_umum" => $req->id_data_umum,
+                "bobot" => $req->bobot,
+                "field_team_konsultan" => $getTeam->field_team_konsultan
+            ]);
+            Storage::putFileAs($this->PATH_FILE_DB, $file, $name);
+            DB::table('detail_laporan_harian_pekerjaan')->insert([
+                "no_trans" => $id,
+                "no_pekerjaan" => $req->no_pekerjaan,
+                "jenis_pekerjaan" => $req->jenis_pekerjaan,
+                "sta_awal" => $req->sta_awal,
+                "sta_akhir" => $req->sta_akhir,
+                "ki_ka" => $req->ki_ka,
+                "volume" => $req->volume,
+                "satuan" => $req->satuan,
+                "ket" => $req->ket,
+                "tgl" => $req->tanggal,
+                "bobot" => $req->bobot
+            ]);
+    
+            if ($req->bahan[0] != null) {
+                for ($i = 0; $i < count($req->bahan); $i++) {
+                    DB::table('detail_laporan_harian_bahan')->insert([
+                        "no_trans" => $id,
+                        "bahan" => $req->bahan[$i],
+                        "volume" => $req->volume_bahan[$i],
+                        "satuan" => $req->satuan_bahan[$i]
+                    ]);
+                }
             }
-        }
-
-
-        if ($req->jenis_peralatan[0] != null) {
-            for ($i = 0; $i < count($req->jenis_peralatan); $i++) {
-                DB::table('detail_laporan_harian_peralatan')->insert([
-                    "no_trans" => $id,
-                    "jenis_peralatan" => $req->jenis_peralatan[$i],
-                    "jumlah" => $req->jumlah_peralatan[$i],
-                    "satuan" => $req->satuan_peralatan[$i]
-                ]);
+    
+    
+            if ($req->jenis_peralatan[0] != null) {
+                for ($i = 0; $i < count($req->jenis_peralatan); $i++) {
+                    DB::table('detail_laporan_harian_peralatan')->insert([
+                        "no_trans" => $id,
+                        "jenis_peralatan" => $req->jenis_peralatan[$i],
+                        "jumlah" => $req->jumlah_peralatan[$i],
+                        "satuan" => $req->satuan_peralatan[$i]
+                    ]);
+                }
             }
-        }
-
-
-        if ($req->bahan_hotmix[0] != null) {
-            for ($i = 0; $i < count($req->bahan_hotmix); $i++) {
-                DB::table('detail_laporan_harian_hotmix')->insert([
-                    "no_trans" => $id,
-                    "bahan_hotmix" => $req->bahan_hotmix[$i],
-                    "no_dt" => $req->no_dt[$i],
-                    "waktu_datang" => $req->waktu_datang[$i],
-                    "waktu_hampar" => $req->waktu_hampar[$i],
-                    "suhu_datang" => $req->suhu_datang[$i],
-                    "suhu_hampar" => $req->suhu_hampar[$i],
-                    "pro_p" => $req->pro_p[$i],
-                    "pro_l" => $req->pro_l[$i],
-                    "pro_t" => $req->pro_t[$i],
-                    "ket" => $req->ket_hotmix[$i]
-                ]);
+    
+    
+            if ($req->bahan_hotmix[0] != null) {
+                for ($i = 0; $i < count($req->bahan_hotmix); $i++) {
+                    DB::table('detail_laporan_harian_hotmix')->insert([
+                        "no_trans" => $id,
+                        "bahan_hotmix" => $req->bahan_hotmix[$i],
+                        "no_dt" => $req->no_dt[$i],
+                        "waktu_datang" => $req->waktu_datang[$i],
+                        "waktu_hampar" => $req->waktu_hampar[$i],
+                        "suhu_datang" => $req->suhu_datang[$i],
+                        "suhu_hampar" => $req->suhu_hampar[$i],
+                        "pro_p" => $req->pro_p[$i],
+                        "pro_l" => $req->pro_l[$i],
+                        "pro_t" => $req->pro_t[$i],
+                        "ket" => $req->ket_hotmix[$i]
+                    ]);
+                }
             }
-        }
-
-
-        if ($req->bahan_beton[0] != null) {
-            for ($i = 0; $i < count($req->bahan_beton); $i++) {
-                DB::table('detail_laporan_harian_beton')->insert([
-                    "no_trans" => $id,
-                    "bahan_beton" => $req->bahan_beton[$i],
-                    "no_tm" => $req->no_tm[$i],
-                    "waktu_datang" => $req->waktu_datang_beton[$i],
-                    "waktu_curah" => $req->waktu_curah[$i],
-                    "slump_test" => $req->slump_test[$i],
-                    "satuan" => $req->satuan_beton[$i],
-                    "ket" => $req->ket_beton[$i]
-                ]);
+    
+    
+            if ($req->bahan_beton[0] != null) {
+                for ($i = 0; $i < count($req->bahan_beton); $i++) {
+                    DB::table('detail_laporan_harian_beton')->insert([
+                        "no_trans" => $id,
+                        "bahan_beton" => $req->bahan_beton[$i],
+                        "no_tm" => $req->no_tm[$i],
+                        "waktu_datang" => $req->waktu_datang_beton[$i],
+                        "waktu_curah" => $req->waktu_curah[$i],
+                        "slump_test" => $req->slump_test[$i],
+                        "satuan" => $req->satuan_beton[$i],
+                        "ket" => $req->ket_beton[$i]
+                    ]);
+                }
             }
-        }
-
-
-        if ($req->tenaga_kerja[0] != null) {
-            for ($i = 0; $i < count($req->tenaga_kerja); $i++) {
-                DB::table('detail_laporan_harian_tkerja')->insert([
-                    "no_trans" => $id,
-                    "tenaga_kerja" => $req->tenaga_kerja[$i],
-                    "jumlah" => $req->jumlah_tk[$i],
-                ]);
+    
+    
+            if ($req->tenaga_kerja[0] != null) {
+                for ($i = 0; $i < count($req->tenaga_kerja); $i++) {
+                    DB::table('detail_laporan_harian_tkerja')->insert([
+                        "no_trans" => $id,
+                        "tenaga_kerja" => $req->tenaga_kerja[$i],
+                        "jumlah" => $req->jumlah_tk[$i],
+                    ]);
+                }
             }
-        }
-
-
-        if ($req->cerah[0] != null) {
-            for ($i = 0; $i < count($req->cerah); $i++) {
-                DB::table('detail_laporan_harian_cuaca')->insert([
-                    "no_trans" => $id,
-                    "cerah" => $req->cerah[$i],
-                    "hujan_ringan" => $req->hujan_ringan[$i],
-                    "hujan_lebat" => $req->hujan_lebat[$i],
-                    "bencana_alam" => $req->bencana_alam[$i],
-                    "lain_lain" => $req->lain_lain[$i],
-                ]);
+    
+    
+            if ($req->cerah[0] != null) {
+                for ($i = 0; $i < count($req->cerah); $i++) {
+                    DB::table('detail_laporan_harian_cuaca')->insert([
+                        "no_trans" => $id,
+                        "cerah" => $req->cerah[$i],
+                        "hujan_ringan" => $req->hujan_ringan[$i],
+                        "hujan_lebat" => $req->hujan_lebat[$i],
+                        "bencana_alam" => $req->bencana_alam[$i],
+                        "lain_lain" => $req->lain_lain[$i],
+                    ]);
+                }
             }
+    
+    
+            DB::table('history_laporan')->insert([
+                "username" => $req->kontraktor,
+                "created_at" => \Carbon\Carbon::now(),
+                "keterangan" => "Laporan Telah Dibuat Oleh " . $req->kontraktor,
+                "user_id" => $req->user,
+                "id_laporan" => $id,
+                "class" => "kirim"
+            ]);
+            DB::commit();
+            return response()->json([
+                'code' => 200
+            ], 200);
+    
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return response()->json([
+                'code' => 500,
+                'message'=>$e->getMessage()
+            ], 500);
         }
-
-
-        DB::table('history_laporan')->insert([
-            "username" => $req->kontraktor,
-            "created_at" => \Carbon\Carbon::now(),
-            "keterangan" => "Laporan Telah Dibuat Oleh " . $req->kontraktor,
-            "user_id" => $req->user,
-            "id_laporan" => $id,
-            "class" => "kirim"
-        ]);
-        return response()->json([
-            'code' => 200
-        ], 200);
-
+        
     }
 
     public function editLaporan(Request $req)
