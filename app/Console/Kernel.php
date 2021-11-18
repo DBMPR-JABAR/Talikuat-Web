4a072b1aedc814a6c41adec70c97322efaacd797
xart22 <fajar.rn22@gmail.com>
2021-11-18 14:23:31 +0700

    need to test cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestEmail;
 
 class Kernel extends ConsoleKernel
 {
@@ -27,7 +29,6 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             try {
-                date_default_timezone_set('Asia/Jakarta');
                 $permintaan = DB::table('request')->get();
                 foreach ($permintaan as $request) {
                     $permintaan_konsultan =  date("d-m-Y", strtotime($request->tgl_kirim_to_konsultan));
@@ -35,33 +36,106 @@ class Kernel extends ConsoleKernel
                     $date_lock_konsultan = date("d-m-Y", strtotime($permintaan_konsultan . '+2 days'));
                     $date_lock_mk = date("d-m-Y", strtotime($permintaan_mk . '+2 days'));
                     if (!empty($request->tgl_kirim_to_konsultan)) {
-                        if ($permintaan_konsultan == date('d-m-Y')) {
+                        if ($permintaan_konsultan == $date_lock_konsultan) {
                             if ($request->status == 2) {
                                 DB::table('request')->where('id', $request->id)->update([
                                     'konsultan' => 'Konsultan Tidak Merespon File diteruskan Ke MK',
                                     'status' => 3,
                                     'bypass_konsultan' => 1,
                                 ]);
+                                DB::table('history_request')->insert([
+                                    "username" => 'System',
+                                    "id_request" => $request->id,
+                                    "user_id" => '1',
+                                    "keterangan" => "Request Telah Telah Diteruskan Oleh System Kepada MK",
+                                    "class" => "kirim",
+                                    "created_at" => \Carbon\Carbon::now()
+                                ]);
+                                $bodyEmail = [
+                                    "role" => "System",
+                                    "status" => "Mem-bypass ",
+                                    "revisi" => "",
+                                    "username" => "Talikuat",
+                                    "no_dokumen" => $request->id,
+                                    "kegiatan" => $request->nama_kegiatan,
+                                    "lokasi" => $request->lokasi_sta,
+                                    "jenis_pekerjaan" => $request->jenis_pekerjaan,
+                                    "volume" => $request->volume,
+                                    "note" => "Konsultan Tidak Merespon Request dan dihandle Oleh System"
+                                ];
+                                $mailto = DB::table('member')->where('perusahaan', '=', $request->nama_direksi)->get();
+                                foreach ($mailto as $email) {
+                                    if ($email->email != null) Mail::to($email->email)->send(new TestEmail($bodyEmail));
+                                }
                             }
                         }
                     }
                     if (!empty($request->tgl_kirim_to_mk)) {
-                        if ($permintaan_mk == date('d-m-Y')) {
+                        if ($permintaan_mk == $date_lock_mk) {
                             if ($request->status == 3) {
                                 DB::table('request')->where('id', $request->id)->update([
                                     'mk' => 'MK Tidak Merespon File diteruskan Ke PPK',
                                     'status' => 4,
                                     'bypass_mk' => 1,
                                 ]);
+                                DB::table('history_request')->insert([
+                                    "username" => 'System',
+                                    "id_request" => $request->id,
+                                    "user_id" => '1',
+                                    "keterangan" => "Request Telah Telah Diteruskan Oleh System Kepada PPK",
+                                    "class" => "kirim",
+                                    "created_at" => \Carbon\Carbon::now()
+                                ]);
+                                $bodyEmail = [
+                                    "role" => "System",
+                                    "status" => "Mem-bypass ",
+                                    "revisi" => "",
+                                    "username" => "Talikuat",
+                                    "no_dokumen" => $request->id,
+                                    "kegiatan" => $request->nama_kegiatan,
+                                    "lokasi" => $request->lokasi_sta,
+                                    "jenis_pekerjaan" => $request->jenis_pekerjaan,
+                                    "volume" => $request->volume,
+                                    "note" => "MK Tidak Merespon Request dan dihandle Oleh System"
+                                ];
+                                $mailto = DB::table('member')->where('perusahaan', '=', $request->nama_direksi)->get();
+                                foreach ($mailto as $email) {
+                                    if ($email->email != null) Mail::to($email->email)->send(new TestEmail($bodyEmail));
+                                }
                             }
                         }
                     }
                 }
                 info('cron job execute [bypass_permintaan]');
+                $laporan_harian = DB::table('master-laporan_harian')->get();
+                foreach ($laporan_harian as $laporan) {
+                    $laporan_konsultan = date("d-m-Y", strtotime($laporan->tgl_kirim_to_konsultan));
+                    $date_lock_konsultan = date("d-m-Y", strtotime($laporan_konsultan . '+2 days'));
+                    if (!empty($laporan->tgl_kirim_to_konsultan)) {
+                        if ($laporan_konsultan == $date_lock_konsultan) {
+                            if ($laporan->status == 2) {
+                                DB::table('master_laporan_harian')->where('no_trans', $laporan->no_trans)->update([
+                                    'konsultan' => 'Konsultan Tidak Merespon, File diteruskan Ke PPK',
+                                    'status' => 3,
+                                    'bypass_konsultan' => 1
+                                ]);
+                                DB::table('history_laporan')->insert([
+                                    "username" => 'System',
+                                    "id_request" => $laporan->id,
+                                    "user_id" => '1',
+                                    "keterangan" => "Laporan Telah Telah Diteruskan Oleh System Kepada PPK",
+                                    "class" => "kirim",
+                                    "created_at" => \Carbon\Carbon::now()
+                                ]);
+                            }
+                        }
+                    }
+                }
+                info('cron job execute [bypass_laporan]');
             } catch (\Throwable $e) {
                 info($e->getMessage());
             }
-        })->everyMinute();
+        })->dailyAt('00:00');
     }
 
     /**
